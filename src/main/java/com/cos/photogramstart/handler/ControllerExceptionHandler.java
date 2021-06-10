2987114b05540f15bdae48091b7e4814d7c4c815
Gandy <gandy0215@naver.com>
2021-06-10 12:39:21 +0900

    feat 공통 응답 스크립트 유틸 생성
    오류 메시지를 alert script로 리턴하는 기능 추가

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,20 +1,18 @@
 package com.cos.photogramstart.handler;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
-import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.util.Script;
 
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<Map<String, String>> vaildationException(CustomValidationException e) {
-		return new CMRespDto(-1, e.getMessage(), e.getErrorMap());
+	public String vaildationException(CustomValidationException e) {
+		return Script.back(e.getErrorMap().toString());
 	}
 }
