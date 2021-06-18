aca339091af91baa3a79542c77b43465e5e85593
Gandy <gandy0215@naver.com>
2021-06-18 13:00:37 +0900

    feat 이미지 업로드 기능 구현
    프로필의 이미지 업로드 기능 구현
    - 파일 저장과 DB저장

---
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,48 @@
+package com.cos.photogramstart.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+	@Value("${file.path}")
+	private String FILE_UPLOAD_PATH;
+
+	private final ImageRepository imageRepository;
+
+	public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+		UUID uuid = UUID.randomUUID();
+		String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename();
+		log.info("이미지 이름 :: " + imageFileName);
+
+		Path imageFilePath = Paths.get(FILE_UPLOAD_PATH + imageFileName);
+
+		try {
+			Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser());
+		Image imageEntity = imageRepository.save(image);
+
+		log.info(imageEntity.toString());
+	}
+}
