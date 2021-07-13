fa342fc6a1c705530b07a2c2607ac00b9413d6aa
Gandy <gandy0215@naver.com>
2021-07-13 13:19:05 +0900

    feat 구독자 리스트 조회하기 서비스 구현
    구독자 리스트 조회하는 서버 로직 구현

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -1,10 +1,17 @@
 package com.cos.photogramstart.service;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.subscribe.SubscribeDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +20,28 @@ import lombok.RequiredArgsConstructor;
 public class SubscribeService {
 
 	private final SubscribeRepository subscribeRepository;
+	private final EntityManager entityManager;
+
+	@Transactional(readOnly = true)
+	public List<SubscribeDto> 구독리스트(int principalId, int pageUSerId) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+		sb.append("if((SELECT 1 FROM Subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, ");
+		sb.append("if((? = u.id), 1, 0) equalUserState ");
+		sb.append("From User u INNER JOIN Subscribe s ");
+		sb.append("ON u.id = s.toUserId ");
+		sb.append("WHERE s.fromUserId = ?");
+
+		Query query = entityManager.createNativeQuery(sb.toString())
+			.setParameter(1, principalId)
+			.setParameter(2, principalId)
+			.setParameter(3, pageUSerId);
+
+		JpaResultMapper resultMapper = new JpaResultMapper();
+		List<SubscribeDto> list = resultMapper.list(query, SubscribeDto.class);
+
+		return list;
+	}
 
 	@Transactional
 	public int 구독하기(int fromUserId, int toUserId) {
@@ -29,6 +58,4 @@ public class SubscribeService {
 	public int 구독취소하기(int fromUserId, int toUserId) {
 		return subscribeRepository.mUnSubscribe(fromUserId, toUserId);
 	}
-
-
 }
--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -1,13 +1,17 @@
 package com.cos.photogramstart.web.api;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.service.SubscribeService;
 import com.cos.photogramstart.service.UserService;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import com.cos.photogramstart.web.dto.user.UserUpdateDto;
+import com.cos.photogramstart.web.subscribe.SubscribeDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +32,15 @@ import lombok.RequiredArgsConstructor;
 public class UserApiController {
 
 	private final UserService userService;
+	private final SubscribeService subscribeService;
+
+	@GetMapping("/api/user/{pageUSerId}/subscribe")
+	public ResponseEntity<?> subscribeList(@PathVariable int pageUSerId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+		List<SubscribeDto> subscribeDtoList = subscribeService.구독리스트(principalDetails.getUser().getId(), pageUSerId);
+
+		return new ResponseEntity<>(new CMRespDto<>(1, "구독자리스트 조회성공", subscribeDtoList), HttpStatus.OK);
+	}
 
 	@PutMapping("/api/user/{id}")
 	public CMRespDto<?> update(@PathVariable int id,
